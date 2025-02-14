e9c60e975c15068187e58a349ed290fade140525
JayKeogh <C00300208@setu.ie>
2025-02-14 09:09:32 +0000

    update db.inc2.php

--- Task2/db.inc2.php
+++ Task2/db.inc2.php
@@ -1,14 +1,18 @@
+
 <?php 
-    $hostname = "localhost";
-    $username = "C00300208";
-    $password = "labuser51";
+    // Define database connection parameters
+    $hostname = "localhost"; // Hostname of the database server
+    $username = "C00300208"; // Username for database authentication
+    $password = "labuser51"; // Password for database authentication
+    $dbname = "MYDBC00300208"; // Name of the database to connect to
 
-    $dbname = "MYDBC00300208";
-
+    // Establish a connection to the MySQL database
     $con = mysqli_connect($hostname, $username, $password, $dbname);
 
-    if(!$con)
-        {
+    // Check if the connection was successful
+    if(!$con) 
+    {
+        // If connection fails, display an error message and terminate the script
         die ("Failed to connect to MySQL: " . mysqli_connect_error());
-        }
+    }
 ?>
