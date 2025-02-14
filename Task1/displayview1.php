f74476b9e99dbb8fc874fb7a81b27abd24aeb522
JayKeogh <C00300208@setu.ie>
2025-02-14 10:17:54 +0000

    Update displayview1.php

--- Task1/displayview1.php
+++ Task1/displayview1.php
@@ -1,12 +1,19 @@
+<!--
+    StudentName:   Jay Keogh
+    Id:            C00300208
+    Lab:           3
+    Task:          2
+-->
+
 <?php
 // Start a new session or resume the existing one
 session_start();
 
 // Include the database connection file
-include 'db.inc.php';
+include 'db.inc1.php';
 
 // Construct SQL query to select a person based on the provided person ID
-// There's an issue here: `$_POST('personiid')` should be `$_POST['personid']`
+// Note: `$_POST('personid')` should be corrected to `$_POST['personid']`
 $sql = "SELECT * FROM persons WHERE personid = " . $_POST['personid'];
 
 // Execute the SQL query and check if it was successful
@@ -19,19 +26,20 @@ if (!$result = mysqli_query($con, $sql))
 // Get the number of rows affected by the query
 $rowcount = mysqli_affected_rows($con);
 
-// There's an issue here: `$_SESSION('personid')` should be `$_SESSION['personid']`
+// Correct usage of session variables: `$_SESSION['personid']` instead of `$_SESSION('personid')`
 $_SESSION['personid'] = $_POST['personid'];
 
+// Check if exactly one record was found
 if ($rowcount == 1) 
 {
     // Fetch the result row as an associative array
     $row = mysqli_fetch_array($result);
 
-    // There's an issue here: `$row('columnName')` should be `$row['columnName']`
-    $_SESSION['personid'] = $row['personId'];
-    $_SESSION['firstname'] = $row['firstName'];
-    $_SESSION['lastname'] = $row['lastName'];
-    $_SESSION['dob'] = $row['DOB'];
+    // Correct array syntax: `$row['columnName']` instead of `$row('columnName')`
+    $_SESSION['personid'] = $row['personId']; // Store person ID in session
+    $_SESSION['firstname'] = $row['firstName']; // Store first name in session
+    $_SESSION['lastname'] = $row['lastName']; // Store last name in session
+    $_SESSION['dob'] = $row['DOB']; // Store date of birth in session
 } 
 else if ($rowcount == 0) 
 {
@@ -47,6 +55,6 @@ mysqli_close($con);
 // Redirect back to the calling form with session variables set, if a record was found
 header('Location: view.html.php');
 
-// Alternatively, redirect using JavaScript (commented out)
+// Alternatively, redirect using JavaScript (this line is commented out)
 // echo "<script>window.location.href = 'view.html.php'</script>";
 ?>
