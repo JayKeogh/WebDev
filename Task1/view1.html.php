244383fdeab94aa382fc4a3984e49ba897ec823d
JayKeogh <C00300208@setu.ie>
2025-02-14 08:59:37 +0000

    view1.html.php

--- Task1/view1.html.php
+++ Task1/view1.html.php
@@ -1,42 +1,64 @@
-<?php session_start(); ?>
+<?php session_start(); ?> <!-- Start or resume a session -->
+
 <html>
 <body>
-<form action="displayview.php" method="Post">
-<p><label for="personid"> Enter the personod you want to find</label>
-<input type="text" name="personid" id="personid" placeholder = "person id" autocomplete=off
-required value="<?php if (ISSET($_SESSION('personid')) )echo $_SESSION('personid')?>"/>
-</p>
-<p><label for="firstname"> First name</label>
-<input type="text" name="firstname" id="firstname" placeholder = "First name" disabled 
-value="<?php if (ISSET($_SESSION('firstname')) )echo $_SESSION('firstname')?>"/>
 
-</p>
-<p><label for="surname"> Last Name</label>
-<input type="text" name="surname" id="surname" placeholder = "Surname" disabled
- value="<?php if (ISSET($_SESSION('lastname')) )echo $_SESSION('lastname')?>"/>
+    <!-- Form to search for a person by ID -->
+    <form action="displayview.php" method="POST"> 
+        <!-- The form submits data to "displayview.php" using the POST method -->
 
-</p>
-<p><label for="dob"> Date of Birth</label>
-<input type="text" name="dob" id="dob" placeholder = "Date of Birth" disabled
-value="<?php if (ISSET($_SESSION('dob')) )echo $_SESSION('dob')?>"/>
-</p>
-<br> <br>
+        <p>
+            <!-- Label for person ID input -->
+            <label for="personid">Enter the person ID you want to find</label>
+            <input type="text" name="personid" id="personid" placeholder="Person ID" 
+                autocomplete="off" required 
+                value="<?php if (isset($_SESSION['personid'])) echo $_SESSION['personid']; ?>"/>
+            <!-- `isset()` is correctly used to check if a session variable exists -->
+            <!-- The value is prefilled with the session variable if available -->
+        </p>
 
-<input type="Submit" value = "Submit" />
-<p>
-</form>
+        <p>
+            <!-- Label for first name -->
+            <label for="firstname">First Name</label>
+            <input type="text" name="firstname" id="firstname" placeholder="First Name" 
+                disabled value="<?php if (isset($_SESSION['firstname'])) echo $_SESSION['firstname']; ?>"/>
+            <!-- `disabled` prevents the user from modifying the value -->
+        </p>
 
-<?php
-if (!ISSET($_SESSION('firstname')) and ISSET($_SESSION('personid')))
+        <p>
+            <!-- Label for last name -->
+            <label for="surname">Last Name</label>
+            <input type="text" name="surname" id="surname" placeholder="Surname" 
+                disabled value="<?php if (isset($_SESSION['lastname'])) echo $_SESSION['lastname']; ?>"/>
+        </p>
+
+        <p>
+            <!-- Label for date of birth -->
+            <label for="dob">Date of Birth</label>
+            <input type="text" name="dob" id="dob" placeholder="Date of Birth" 
+                disabled value="<?php if (isset($_SESSION['dob'])) echo $_SESSION['dob']; ?>"/>
+        </p>
+
+        <br><br>
+
+        <!-- Submit button to send person ID for searching -->
+        <input type="submit" value="Submit"/>
+
+    </form> <!-- Correctly closed form tag -->
+
+    <?php
+    // Check if no first name is found but a person ID exists
+    if (!isset($_SESSION['firstname']) && isset($_SESSION['personid'])) 
     {
-    echo '<p style="Color: red; text-align: center; font-size:20">
-        No record found for a person with id..' . $_SESSION('personid') . '
-        <br> Please try again!
-        </p>';
+        echo '<p style="color: red; text-align: center; font-size: 20px;">
+            No record found for a person with ID ' . $_SESSION['personid'] . '
+            <br> Please try again!
+            </p>';
 
-    unset ($_SESSION('personid'));
+        // Remove the session variable for person ID since no record was found
+        unset($_SESSION['personid']);
     }
+    ?>
 
-?>
 </body>
 </html>
